99123e804f4fdaec342f645a5c7a951fb0a20780
Makhafola Thato Daniel <59382287+thato421@users.noreply.github.com>
2024-07-29 13:35:17 +0200

    Added Calculator Files

---
+++ main.js
@@ -0,0 +1,113 @@
+// Main JS File
+
+const calcInput = document.querySelector(".calc_input");
+
+const calcOutput = document.querySelector(".calc_output");
+
+function addOne(number){
+
+    calcInput.value += '1';
+
+}
+function addTwo(number){
+
+    calcInput.value += '2';
+
+}
+function addThree(number){
+
+    calcInput.value += '3';
+
+}
+function addPlus(){
+
+    calcInput.value += '+';
+
+}
+function addDoubleZero(number){
+
+    calcInput.value += '00';
+
+}
+function addZero(number){
+
+    calcInput.value += '0';
+
+}
+function addPeriod(){
+
+    calcInput.value += '.';
+
+}
+function addMinus(){
+
+    calcInput.value += '-'
+
+}
+function addSix(number){
+
+    calcInput.value += '6';
+
+}
+function addFive(number){
+
+    calcInput.value += '5';
+
+}
+function addFour(number){
+
+    calcInput.value += '4';
+
+}
+function addMultiply(){
+
+    calcInput.value += '*';
+
+}
+function addNine(number){
+
+    calcInput.value += '9';
+
+}
+function addEight(number){
+
+    calcInput.value += '8';
+
+}
+function addSeven(number){
+
+    calcInput.value += '7';
+
+}
+function addDivide(){
+
+    calcInput.value += '/';
+
+}
+function addPercent(){
+
+    calcInput.value += '%';
+
+}
+function evalInput(){
+
+    try{
+    calcOutput.value = eval(calcInput.value);
+    }
+    catch (e){
+        calcOutput.value = ('Invalid Format');
+    }
+
+}
+function delChar(){
+
+    calcInput.value = calcInput.value.slice(0, -1);
+
+}
+function clearInputOutput(){
+
+    calcInput.value = '';
+
+    calcOutput.value = '';
+
+}
